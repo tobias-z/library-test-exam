c3183d8f93cc5a82cf1fb47696097919d76e848e
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-06-10 18:27:58 +0200

    test: maybe it works now :)

--- server/src/test/java/utils/TestUtils.java
+++ server/src/test/java/utils/TestUtils.java
@@ -6,10 +6,10 @@ public class TestUtils {
 
     public static void dropTables(EntityManager em) {
         em.createNamedQuery("Loan.deleteAllRows").executeUpdate();
-        em.createNamedQuery("Book.deleteAllRows").executeUpdate();
-        em.createNamedQuery("Library.deleteAllRows").executeUpdate();
         em.createQuery("delete from User").executeUpdate();
         em.createQuery("delete from Role").executeUpdate();
+        em.createNamedQuery("Book.deleteAllRows").executeUpdate();
+        em.createNamedQuery("Library.deleteAllRows").executeUpdate();
     }
 
 }
